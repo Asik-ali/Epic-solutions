bb91579e85ac50999e8a1d80a953100f557acd49
Asik-ali <asik14923@gmail.com>
2024-05-23 23:01:58 +0530

    As

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,7 +8,7 @@ function Home() {
     <>
       <Hero />
       <Welcome />
-      <div className='flex justify-evenly'>
+      <div className='flex justify-evenly flex-wrap'>
       <AdSense client="ca-pub-7832822790443742" slot="5" width={320} height={480} />
       <AdSense client="ca-pub-7832822790443742" slot="6" width={336} height={280} />
       <AdSense client="ca-pub-7832822790443742" slot="7" width={480} height={320} />
