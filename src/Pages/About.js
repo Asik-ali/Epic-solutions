7492a109678886a77adf9ae5360288428e01566c
Asik-ali <asik14923@gmail.com>
2024-06-05 21:30:41 +0530

    A

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import AboutContent from '../Components/About/About'
+import Adsense from './Adsense'
 
 function About() {
   return (
     <div>
-        <AboutContent/>
+      <Adsense client="ca-pub-7832822790443742" slot="5" width={320} height={480} />
+      <AboutContent />
     </div>
   )
 }
