255ac786839737db10f9198cfc7bbadc366fd8f1
Asik-ali <asik14923@gmail.com>
2024-06-05 21:17:52 +0530

    A

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Hero from '../../src/Components/Hero/Hero';
 import Welcome from '../Components/Welcome/Welcome';
-import AdSense from './AdSense'; // Adjust the import path accordingly
+import Adsense from './Adsense'; // Adjust the import path accordingly
 
 function Home() {
   return (
@@ -9,9 +9,9 @@ function Home() {
       <Hero />
       <Welcome />
       <div className='flex justify-evenly flex-wrap'>
-        <AdSense client="ca-pub-7832822790443742" slot="5" width={320} height={480} />
-        <AdSense client="ca-pub-7832822790443742" slot="6" width={336} height={280} />
-        <AdSense client="ca-pub-7832822790443742" slot="7" width={480} height={320} />
+        <Adsense client="ca-pub-7832822790443742" slot="5" width={320} height={480} />
+        <Adsense client="ca-pub-7832822790443742" slot="6" width={336} height={280} />
+        <Adsense client="ca-pub-7832822790443742" slot="7" width={480} height={320} />
       </div>
     </>
   );
