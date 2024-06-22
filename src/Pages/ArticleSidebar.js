9bef3c3dcd017cbd84cbd2737048641980da3754
Asik-ali <asik14923@gmail.com>
2024-06-22 11:36:42 +0530

    A

--- src/Pages/ArticleSidebar.js
+++ src/Pages/ArticleSidebar.js
@@ -22,7 +22,8 @@ const ArticleSidebar = () => {
                                 <Adsense client="ca-pub-7832822790443742" slot="5" width={320} height={480} />
                                 <Adsense client="ca-pub-7832822790443742" slot="6" width={336} height={280} />
                             </>
-                        )}{isTabletView() && (
+                        )}
+                        {/* {isTabletView() && (
                             <>
                                 <Adsense client="ca-pub-7832822790443742" slot="7" width={728} height={90} />
                                 <Adsense client="ca-pub-7832822790443742" slot="8" width={768} height={90} />
@@ -33,7 +34,7 @@ const ArticleSidebar = () => {
                                 <Adsense client="ca-pub-7832822790443742" slot="9" width={1024} height={90} />
                                 <Adsense client="ca-pub-7832822790443742" slot="10" width={1200} height={90} />
                             </>
-                        )}
+                        )} */}
                     </div>
       </div>
     </div>
